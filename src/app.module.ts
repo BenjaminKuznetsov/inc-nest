6d3ea14fbdb851ddaaa9d890c6307d7b770652be
BenjaminKuznetsov <benjaminkuznetsoff@gmail.com>
2025-02-07 15:48:28 +0300

    testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserAccountsModule } from './modules/user-accounts/user-accounts.module';
 import { BloggersPlatformModule } from './modules/bloggers-platform/bloggers-platform.module';
-import { PostsModule } from './src/modules/bloggers-platform/posts/posts.module';
+import { TestingModule } from './modules/testing/testing.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-blogger-platform'),
     UserAccountsModule,
     BloggersPlatformModule,
-    PostsModule,
+    TestingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/modules/testing/testing.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Injectable()
+export class TestingService {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  async dropDatabase(): Promise<void> {
+    if (!this.connection || !this.connection.db) {
+      throw new Error('Подключение к базе данных отсутствует');
+    }
+
+    try {
+      await this.connection.db.dropDatabase();
+      console.log('База данных успешно удалена');
+    } catch (error) {
+      console.error('Ошибка при удалении базы данных:', error);
+      throw error;
+    }
+  }
+}
