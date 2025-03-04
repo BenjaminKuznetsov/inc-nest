c30f5ba0887b4d932e83de7ae16380a66d6cae3f
BenjaminKuznetsov <benjaminkuznetsoff@gmail.com>
2025-03-04 18:35:52 +0300

    vercel

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -4,6 +4,7 @@ import { pipeSetup } from './pipe-setup';
 import { swaggerSetup } from './swagger-setup';
 
 export function appSetup(app: INestApplication) {
+  app.enableCors();
   pipeSetup(app);
   prefixSetup(app);
   swaggerSetup(app);
